03be0babb013cb953e588b33087bee71cf902bdd
lizzymak <146508325+lizzymak@users.noreply.github.com>
2026-02-02 12:30:49 -0800

    get_majors and major_requirements api endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastApi 
+from fastapi import FastAPI 
+import requests
 
-app = FastApi()
+app = FastAPI()
+BASE_URL = "https://anteaterapi.com/v2/rest"
 
 @app.get("/")
 def root():
     return {"message": "Backend is running"}
+
+@app.get("/majors")
+def get_all_majors():
+    #params = {"division": "Undergraduate"}
+    response = requests.get(f"{BASE_URL}/programs/majors")
+    return response.json()
+
+@app.get("/major_requirements")
+def get_major_requirements(programId: str):
+    response = requests.get(f"{BASE_URL}/programs/major", params={"programId": programId})
+    return response.json()
